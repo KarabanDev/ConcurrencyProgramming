da6e835c8f586024866efaf5f76ee86baaeff0ec
Andrey Karaban <andrey.karaban@icloud.com>
2015-12-21 20:12:16 +0500

    Initial Commit

---
+++ ExampleOfConcurrency/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExampleOfConcurrency
+//
+//  Created by Andrey Karaban on 21.12.15.
+//  Copyright © 2015 Andrey Karaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
